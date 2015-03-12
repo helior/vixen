93364517acd8a979c5de1f601948ebc0a26a55ac
helior <me@helior.info>
2015-03-12 16:21:06 -0700

    FAQ web service endpoint exposed.

---
+++ src/vixen/modules/features/vixen_faq/plugins/restful/restful/faq/1.0/VixenFaq.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains AtlasEpisodes.
+ */
+
+class VixenFaq extends RestfulEntityBaseNode {
+  /**
+   * {@inheritdoc}
+   */
+  public function publicFieldsInfo() {
+    $fields = parent::publicFieldsInfo();
+
+    return $fields;
+  }
+}
