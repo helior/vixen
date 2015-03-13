1e7e88827511b869eb552fad1cc0910ad820ec0e
helior <me@helior.info>
2015-03-12 17:09:43 -0700

    Exposing questions, answer, and category in faq api

--- src/vixen/modules/features/vixen_faq/plugins/restful/restful/faq/1.0/VixenFaq.class.php
+++ src/vixen/modules/features/vixen_faq/plugins/restful/restful/faq/1.0/VixenFaq.class.php
@@ -11,6 +11,9 @@ class VixenFaq extends RestfulEntityBaseNode {
    */
   public function publicFieldsInfo() {
     $fields = parent::publicFieldsInfo();
+    $fields['questions'] = array('property' => 'field_faq_questions');
+    $fields['answer'] = array('property' => 'field_faq_answer', 'sub_property' => 'value');
+    $fields['category'] = array('property' => 'field_faq_category', 'sub_property' => 'name');
 
     return $fields;
   }
